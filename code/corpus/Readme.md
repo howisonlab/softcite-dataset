ef3fc2101fd5e2caa39a15c2eaf17cbab05b2254
lopez <patrice.lopez@science-miner.com>
2020-08-14 15:04:48 +0200

    add corpus data, scripts and original TEI

---
+++ code/corpus/Readme.md
@@ -0,0 +1,143 @@
+# Some python scripts for analyzing and manipulating the corpus data
+
+## Analysis of training data consistency
+
+ Python 3.* script to analyse XML training data and spot possible unconsistencies to review. To launch the script: 
+
+```console
+> python3 consistency.py _absolute_path_to_training_directory_
+```
+
+All the `*.xml` files in the `_absolute_path_to_training_directory_` will be analuzed. For instance for the Softcite corpus files: 
+
+
+```console
+> python3 consistency.py ../../data/corpus/
+```
+
+For more details, see the description of the output directly in the header of the `consistency.py` file. 
+
+
+## Generate some JSON working formats
+
+To simplify the usage of TEI XML and annotated documents, some simplified lossy JSON formats are produced by the following scripts.
+
+### Generate TEI XML for a set of PDF
+
+Given a set of PDF files, for instance the PDF of the Softcite corpus, GROBID cam extract and structure automatically their content to facilitate text mining and corpus annotations. 
+
+* install and start a Grobid server
+
+* install the python client
+
+> git clone https://github.com/kermitt2/grobid-client-python
+
+> cd grobid-client-python
+
+* convert PDF files, the following command for instance will convert the PDF under the `--input` path and write TEI XML results under the `--output` path. using a concurrency of 6 parallel conversion (to adapt according to the number of available threads on your machine): 
+
+> python3 grobid-client.py --input ~/softcite-dataset/pdf/  --output ~/softcite-dataset/tei/ --n 6 processFulltextDocument 
+
+
+### Conversion of TEI XML into lossy JSON 
+
+The target JSON format is relatively similar to the CORD-19 format, without the bibliographical data. The idea is capture only what is required for using human annotation tools from the rich TEI XML files. 
+
+```
+usage: TEI2LossyJSON.py [-h] [--tei-file TEI_FILE] [--tei-corpus TEI_CORPUS]
+                        [--output OUTPUT]
+
+Convert a TEI XML file into CORD-19-style JSON format
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --tei-file TEI_FILE   path to a TEI XML file to convert
+  --tei-corpus TEI_CORPUS
+                        path to a directory of TEI XML files to convert
+  --output OUTPUT       path to an output directory where to write the
+                        converted TEI XML file, default is the same directory
+                        as the input file
+
+```
+
+For instance, converting the set of around 5000 Softcite corpus TEI XML files produced by Grobid from the PDF (using typically a client like the [GROBID python client](https://github.com/kermitt2/grobid-client-python)):
+
+> python3 TEI2LossyJSON.py --tei-corpus ~/tools/softcite-dataset/tei/ --output ~/tmp/
+
+In these JSON files, the segmentation in paragraphs as present in the TEI XML files are preserved. 
+
+### Adding the Softcite annotations into the JSON lossy format
+
+Install the dependency for sentence segmentation (using the pragmatic-segmenter port to Python, https://github.com/diasks2/pragmatic_segmenter), use:
+
+> pip3 install -r requirements.txt
+
+```
+usage: corpus2JSON.py [-h] [--tei-corpus TEI_CORPUS] 
+                      [--json-repo JSON_REPO] [--output OUTPUT]
+
+Convert a TEI XML file with Software mention annotations into CORD-19-style
+JSON format
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --tei-corpus TEI_CORPUS
+                        path to the TEI corpus file corresponding to the
+                        Softcite annotated corpus to convert
+  --json-repo JSON_REPO
+                        path to the directory of JSON files converted from TEI
+                        XML produced by GROBID and used in the Softcite corpus
+  --output OUTPUT       path to an output directory where to write the
+                        converted JSON file(s), default is the same directory
+                        as the input file
+```
+
+Having produced the JSON files as indicated above (under `~/tmp/`), we can inject the Softcite corpus curated annotations into the JSON as follow:
+
+> python3 corpus2JSON.py  --tei-corpus ~/tools/softcite-dataset/data/corpus/softcite_corpus.tei.xml --json-repo ~/tmp/ --output ~/tools/softcite-dataset/data/json
+
+
+The script realign the annotations from the excerpts present in the corpus file into the complete JSON document and segment the paragraphs into sentences. The added annotations are present in each JSON sentence elements introduced by the `entity_spans` key.
+
+```json
+{
+    "text": "Images were contrast enhanced based on visual inspection, and individual nuclei were manually delineated in Photoshop 7.0 (Adobe, San Jose, CA), with each nucleus saved in a separate image file. ",
+    "section": "Image acquisition and FISH analysis",
+    "entity_spans": [
+        {
+            "start": 108,
+            "end": 117,
+            "type": "software",
+            "rawForm": "Photoshop",
+            "resp": "#annotator12",
+            "used": true,
+            "id": "c6ecc44fa8-software-0"
+        },
+        {
+            "start": 118,
+            "end": 121,
+            "type": "version",
+            "rawForm": "7.0",
+            "resp": "#annotator12",
+            "id": "#c6ecc44fa8-software-0"
+        },
+        {
+            "start": 123,
+            "end": 128,
+            "type": "publisher",
+            "rawForm": "Adobe",
+            "resp": "#curator",
+            "id": "#c6ecc44fa8-software-0"
+        }
+    ]
+}
+```
+
+__Note:__ to preserve the sentence element order, python scripts must use `OrderedDict` and not usual `dict`, e.g. for loading a json file:
+
+```
+with open(os.path.join(path_json_repo, file)) as jsonfile:
+    json_doc = json.load(jsonfile, object_pairs_hook=OrderedDict)
+``` 
+
+The loaded `json_doc` will be of type `OrderedDict` and the order of sentences in the document will be preserved. 
---
+++ code/corpus/corpus2JSON.py
@@ -0,0 +1,349 @@
+"""
+    Inject software mention annotations from the Softcite Corpus in TEI corpus format into into the JSON format corresponding to
+    something similar to the CORD-19 degraded JSON format.
+"""
+
+import os
+import argparse
+import ntpath
+import json
+import xml
+from xml.sax import make_parser, handler
+from collections import OrderedDict
+from TEI2LossyJSON import TEIContentHandler, convert_tei_string
+import pysbd
+
+class TEICorpusHandler(xml.sax.ContentHandler):
+    """ 
+    TEI XML SAX handler for reading the TEI corpus file corresponding to the Softcite corpus  
+    """
+    json_path = None
+    output_path = None
+
+    # working variable 
+    # working variables
+    accumulated = ''
+    currentOffset = -1
+    is_origin_file = False
+    origin_file = None
+    grobid_json = None
+    paragraph = None
+    current_reference = None
+    current_entity = None
+    ref_spans = None
+    entity_spans = None
+
+    document = None
+    nb_file = 0
+    nb_unmatched_file = 0
+
+    def __init__(self, json_path, output_path):
+        xml.sax.ContentHandler.__init__(self)
+        self.output_path = output_path
+        self.json_path = json_path
+
+    def startElement(self, name, attrs):
+        if self.accumulated != '':
+            if self.paragraph == None:
+                self.paragraph = ''
+            self.paragraph += self.accumulated
+            self.currentOffset += len(self.accumulated)
+            self.accumulated = ''
+        if name == 'idno':
+            if attrs.getLength() != 0:
+                if "type" in attrs:
+                    if attrs.getValue("type") == 'origin':
+                        # this is where we get the PDF file name
+                        self.is_origin_file = True
+        if name == 'TEI' or name == 'tei':
+            # reinit working variable
+            self.is_origin_file = False
+            self.grobid_json = None
+            self.document = OrderedDict() 
+            self.nb_file += 1
+        if name == "body":
+            self.document["body_text"] = []
+            self.accumulated = ''
+        if name == "ref": 
+            if attrs.getLength() != 0:
+                if "type" in attrs:
+                    self.current_reference = OrderedDict() 
+                    if attrs.getValue("type") == 'bibr':
+                        self.current_reference["type"] = "bibr"
+                    if attrs.getValue("type") == 'table':
+                        self.current_reference["type"] = "table"
+                    if attrs.getValue("type") == 'figure':
+                        self.current_reference["type"] = "figure"
+                    if "target" in attrs:
+                        if attrs.getValue("target") != None:
+                            self.current_reference["ref_id"] = attrs.getValue("target").replace("#", "")
+                    self.current_reference["start"] = self.currentOffset
+        if name == "p": 
+            self.paragraph = ''
+            self.ref_spans = []
+            self.entity_spans = []            
+            self.currentOffset = 0
+        if name == "rs":
+            # beginning of entity
+            self.current_entity = OrderedDict() 
+            if attrs.getLength() != 0:
+                if "type" in attrs:
+                    self.current_entity["type"] = attrs.getValue("type")
+                if "resp" in attrs:
+                    self.current_entity["resp"] = attrs.getValue("resp")
+                if "subtype" in attrs:
+                    if attrs.getValue("subtype") == "used":
+                        self.current_entity["used"] = True
+                if "xml:id" in attrs:
+                    self.current_entity["id"] = attrs.getValue("xml:id")
+                if "xml:id" in attrs:
+                    self.current_entity["id"] = attrs.getValue("xml:id")
+                if "corresp" in attrs:
+                    self.current_entity["id"] = attrs.getValue("corresp")
+                self.current_entity["start"] = self.currentOffset
+
+    def endElement(self, name):
+        # print("endElement '" + name + "'")
+        if name == 'idno':
+            if self.is_origin_file:
+                self.origin_file = self.accumulated.strip()
+                json_file_path = os.path.join(self.json_path, self.origin_file + ".json")
+                print(json_file_path)
+
+                if not os.path.isfile(json_file_path):
+                    print("no raw json file available for", self.origin_file + ".json")
+                else:
+                    with open(json_file_path,"r") as f:
+                        grobid_json_string = f.read() 
+                self.grobid_json = json.loads(grobid_json_string, object_pairs_hook=OrderedDict)
+        if name == "rs":
+            self.paragraph += self.accumulated.strip()
+            # end of entity
+            self.current_entity["rawForm"] = self.accumulated.strip()
+            self.current_entity["end"] = self.currentOffset + len(self.accumulated.strip())
+            self.entity_spans.append(self.current_entity)
+            self.current_entity = None
+        if name == "p":
+            if self.document is not None:
+                if self.paragraph == None:
+                    self.paragraph = ''
+                self.paragraph += self.accumulated
+                local_paragraph = OrderedDict() 
+                local_paragraph['text'] = self.paragraph
+                if len(self.ref_spans) > 0:
+                    local_paragraph['ref_spans'] = self.ref_spans
+                if len(self.ref_spans) > 0:
+                    local_paragraph['entity_spans'] = self.entity_spans
+                if not "body_text" in self.document:
+                    self.document["body_text"] = []
+                self.document["body_text"].append(local_paragraph)
+        if name == 'ref':
+            self.paragraph += self.accumulated.strip()
+            if self.current_reference is not None:
+                self.current_reference["text"] = self.accumulated.strip()
+                self.current_reference["end"] = self.currentOffset + len(self.accumulated.strip())
+                self.ref_spans.append(self.current_reference)
+            self.current_reference = None
+        if name == "body":
+            # inject new annotated paragraph
+            for para in self.document["body_text"]:
+                local_text = para["text"]
+                local_text_simplified = local_text.replace(" ", "")
+                local_text_simplified = signature(local_text)
+                #print(local_text)
+                if not self.grobid_json is None and "body_text" in self.grobid_json:
+                    local_match = False
+                    i = 0
+                    for candidate_text in self.grobid_json["body_text"]:
+                        #print(candidate_text["text"])
+                        ind = -1
+                        candidate_string = signature(candidate_text["text"])
+                        if local_text_simplified == candidate_string:
+                            ind = 0
+                        if ind == -1:
+                            try:
+                                ind = local_text_simplified.index(candidate_string)
+                            except:
+                                ind = -1
+                        if ind == -1:
+                            try:
+                                ind = candidate_string.index(local_text_simplified)
+                            except:
+                                ind = -1 
+                        if ind != -1:
+                            #print("match", local_text)
+                            local_match = True
+                            candidate_text["text"] = local_text
+                            if "entity_spans" in para:
+                                #candidate_text["entity_spans"] = para["entity_spans"]
+                                #print(para["entity_spans"])
+                                self.grobid_json["body_text"][i]["entity_spans"] = para["entity_spans"]
+                            if "ref_spans" in para:    
+                                #candidate_text["ref_spans"] = para["ref_spans"]
+                                self.grobid_json["body_text"][i]["ref_spans"] = para["ref_spans"]
+                            break
+                        i += 1
+                    if not local_match:
+                        print("no match:", self.origin_file)
+                        #print(local_text)
+                        self.nb_unmatched_file += 1
+
+            self.grobid_json = convert_to_sentence_segments(self.grobid_json)
+
+            # and write it
+            if self.output_path is None:
+                output_file = self.origin_file + ".json"
+            else:
+                output_file = os.path.join(self.output_path, self.origin_file + ".json")
+            print(output_file)
+            with open(output_file, 'w') as outfile:
+                json.dump(self.grobid_json, outfile, indent=4)
+
+        if name == 'teiCorpus':
+            print("total unmatched", self.nb_unmatched_file, "/", self.nb_file)
+
+        self.currentOffset += len(self.accumulated)
+        self.accumulated = ''
+
+    def characters(self, content):
+        self.accumulated += content
+
+    def getDocument(self):
+        return self.grobid_json
+
+    def clear(self): # clear the accumulator for re-use
+        self.accumulated = ""
+
+def signature(string):
+    string = string.replace("-", "")
+    string = string.replace("\t", "")
+    string = string.replace("\n", "")
+    #string = ''.join([i if ord(i) < 128 else '' for i in string])
+    return string.replace(" ", "")
+
+def convert_to_sentence_segments(json):
+    new_json = OrderedDict()
+    if json is None:
+        return new_json
+    # the abstract is empty for softcite corpus
+    new_json["abstract"] = []
+    new_json["body_text"] = []
+    seg = pysbd.Segmenter(language="en", clean=False, char_span=True)
+    # ['My name is Jonas E. Smith.', 'Please turn to p. 55.']
+    if "body_text" in json:
+        for text_part in json["body_text"]:
+            if "text" in text_part:
+                sentences = seg.segment(text_part["text"])
+                offset_pos = 0
+                # the following is to cancel a sentence segmentation because it is located in the middle of an existing span
+                # if previous_start is -1, previous segmentation was correct
+                previous_start = -1
+                for span in sentences:
+                    if previous_start != -1:
+                        span.start = previous_start
+                        previous_start = -1
+
+                    offset_pos = span.start
+                    #print(span.start, span.end)
+                    sentence_structure = OrderedDict()
+
+                    sentence_structure["text"] = text_part["text"][span.start:span.end]
+                    if "section" in text_part:
+                        sentence_structure["section"] = text_part["section"]
+                    if "ref_spans" in text_part:
+                        new_ref_spans = []
+                        for ref_span in text_part["ref_spans"]:
+                            # check if we have a segmentation in the middle of a ref span
+                            if ref_span["start"] >= offset_pos and ref_span["start"] < span.end and ref_span["end"] > span.end:
+                                """
+                                print("\nwarning, segmentation in the middle of ref span: sentence at", 
+                                    span.start, span.end, "with ref at", ref_span["start"], ref_span["end"])
+                                print("sentence:", text_part["text"][span.start:span.end])
+                                print("ref:", text_part["text"][ref_span["start"]:ref_span["end"]])
+                                print("\n")
+                                """
+                                # in this case, we cancel this sentence boundary
+                                previous_start = span.start
+                                break
+
+                            if ref_span["start"] >= offset_pos and ref_span["end"] <= span.end:
+                                new_ref_span = OrderedDict()
+                                new_ref_span["start"] = ref_span["start"] - offset_pos
+                                new_ref_span["end"] = ref_span["end"] - offset_pos
+                                if "type" in ref_span:
+                                    new_ref_span["type"] = ref_span["type"]
+                                if "ref_id" in ref_span:
+                                    new_ref_span["ref_id"] = ref_span["ref_id"]
+                                if "text" in ref_span:
+                                    new_ref_span["text"] = ref_span["text"]
+                                new_ref_spans.append(new_ref_span)
+                        if len(new_ref_spans) > 0 and previous_start == -1:
+                            sentence_structure["ref_spans"] = new_ref_spans
+
+                    if "entity_spans" in text_part and previous_start == -1:
+                        new_entity_spans = []
+                        for entity_span in text_part["entity_spans"]:
+                            # check if we have a segmentation in the middle of an entity span
+                            if entity_span["start"] >= offset_pos and entity_span["start"] < span.end  and entity_span["end"] > span.end:
+                                """
+                                print("\nwarning, segmentation in the middle of entity span: sentence at", 
+                                    span.start, span.end, "with entity at", entity_span["start"], entity_span["end"])
+                                print("sentence:", text_part["text"][span.start:span.end])
+                                print("entity:", text_part["text"][entity_span["start"]:entity_span["end"]])
+                                print("\n")
+                                """
+                                # in this case, we cancel this sentence boundary
+                                previous_start = span.start
+                                break
+
+                            if entity_span["start"] >= offset_pos and entity_span["end"] <= span.end:
+                                new_entity_span = OrderedDict()
+                                new_entity_span["start"] = entity_span["start"] - offset_pos
+                                new_entity_span["end"] = entity_span["end"] - offset_pos
+                                if "type" in entity_span:
+                                    new_entity_span["type"] = entity_span["type"] 
+                                if "rawForm" in entity_span:
+                                    new_entity_span["rawForm"] = entity_span["rawForm"]
+                                if "resp" in entity_span:
+                                    new_entity_span["resp"] = entity_span["resp"]
+                                if "used" in entity_span:
+                                    new_entity_span["used"] = entity_span["used"]
+                                if "id" in entity_span:
+                                    new_entity_span["id"] = entity_span["id"]
+                                new_entity_spans.append(new_entity_span)
+                        if len(new_entity_spans) >0 and previous_start == -1:
+                            sentence_structure["entity_spans"] = new_entity_spans
+
+                    if previous_start == -1:
+                        new_json["body_text"].append(sentence_structure)
+
+    return new_json
+
+def convert_corpus(tei_corpus_path, json_path, output_path):
+    parser = make_parser()
+    handler = TEICorpusHandler(json_path, output_path)
+    parser.setContentHandler(handler)
+    parser.parse(tei_corpus_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description = "Convert a TEI XML file with Software mention annotations into CORD-19-style JSON format")
+    parser.add_argument("--tei-corpus", type=str, 
+        help="path to the TEI corpus file corresponding to the Softcite annotated corpus to convert")
+    parser.add_argument("--json-repo", type=str, 
+        help="path to the directory of JSON files converted from TEI XML produced by GROBID and used in the Softcite corpus")
+    parser.add_argument("--output", type=str, 
+        help="path to an output directory where to write the converted JSON file(s), default is the same directory as the input file")
+
+    args = parser.parse_args()
+    tei_corpus_path = args.tei_corpus
+    json_repo = args.json_repo
+    output_path = args.output
+
+    # check path and call methods
+    if tei_corpus_path is None or not os.path.isfile(tei_corpus_path):
+        print("the path to the TEI corpus file is not valid: ", tei_corpus_path)
+    if json_repo is None or not os.path.isdir(json_repo):
+        print("the path to the JSON files is not valid: ", json_repo)
+
+    convert_corpus(tei_corpus_path, json_repo, output_path)
