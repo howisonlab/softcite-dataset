ab24c3b81e03319d218cd9edc0e89c1a6b709979
caifand <cfdu@utexas.edu>
2020-08-24 03:05:14 -0500

    reselection bypassing articles with mismatches during json conversion

--- code/corpus/select-rich-mentions.R
+++ code/corpus/select-rich-mentions.R
@@ -5,11 +5,28 @@
 library(tidyverse)
 library(xml2)
 library(here)
+library(stringr)
 
 tei_doc <- read_xml("https://raw.githubusercontent.com/howisonlab/softcite-dataset/master/data/corpus/softcite_corpus-full.tei.xml") %>% xml_ns_strip()
 
+article_id <- xml_find_all(tei_doc, xpath="//fileDesc/@xml:id")
+origin_id <- xml_find_all(tei_doc, xpath="//idno[@type='origin']")
+
+source_corpus <- read_xml("https://raw.githubusercontent.com/ourresearch/software-mentions/master/resources/dataset/software/corpus/all_clean.tei.xml") %>% xml_ns_strip()
+
+article_id <- xml_find_all(tei_doc, xpath="//fileDesc/@xml:id")
+
+article_no_origin <- xml_find_all(tei_doc,xpath="//fileDesc[not(.//idno[@type='origin'])]/@xml:id")
+article_no_origin %>% head(2)
+
+article_with_origin <- xml_find_all(tei_doc,xpath="//fileDesc//idno[@type='origin'] and ../../../@xml:id")
+
+articles_no_origin <- tibble(text = xml_text(article_no_origin), attrs = xml_attrs(article_no_origin)) %>% 
+  unnest_wider(attrs)
+
+
 # Auditing corpus annotations
-n_software <- xml_find_num(tei_doc, xpath="count(//rs[@type='software'])")
+n_software <- xml_find_num(source_corpus, xpath="count(//rs[@type='software'])")
 n_attrs <- xml_find_num(tei_doc, xpath="count(//rs[not(@type='software')])")
 n_anno <- xml_find_num(tei_doc, xpath="count(//rs)")
 n_article <- xml_find_num(tei_doc, xpath="count(//fileDesc[@xml:id])")
@@ -112,25 +129,59 @@ mentions_p <- software_p %>% left_join(version_p, by="id") %>%
   mutate(article_set = 
            if_else(article %in% pmc_id, "PMC", "Economics")) 
 
+# excluding articles missing paragraphs during JSON conversion
+mismatches <- read_lines("C:/Users/D/Documents/Academics/1-Projects/HowisonLab/softcite/softcite-dataset/code/corpus/paragraph_matching_issues.txt")
+
+mismatch_list <- as_tibble_col(mismatches, column_name = "text") %>% 
+  filter(str_detect(text, "no match in")) %>% 
+  mutate(article=str_sub(text, start=13)) %>% 
+  select(article)
+
+# find xml:id of articles with paragraph mismatches
+article_list <- read_csv("sample-csv.csv") %>% 
+  mutate(tag=str_remove(tag, "\\{http\\:\\/\\/www.tei-c.org\\/ns\\/1.0\\}")) %>% 
+  filter(tag %in% c("title", "idno")) %>% 
+  mutate(group_var = if_else(tag == "idno", str_sub(structure_xpath, 1, -13), str_sub(structure_xpath, 1, -8))) 
+
+pmc_list <- article_list %>% 
+  filter(attr %in% pmc_id) %>% 
+  select(group_var) 
+# 2,505 PMC articles in parsed results
+# seems losing 16 articles but does not affect the sampling below
+
+sample_frame <- pmc_list %>% 
+  left_join(article_list, by="group_var") %>% 
+  filter(attr == "PMC") %>% 
+  left_join(article_list, by="group_var") %>% 
+  filter(tag.y == "title") %>% 
+  select(group_var, text.x, attr.y)
+
+mismatch_list <- mismatch_list %>% 
+  left_join(sample_frame, by=c("article"="text.x")) %>% 
+  pull(attr.y)
+  
+
 # Selecting rich mentions
 all_mentioned <- mentions_p %>% 
+  filter(!article %in% mismatch_list) %>% 
   filter(!is.na(version1) & !is.na(publisher1) & !is.na(url)) %>% 
-  # 9 software mentioned with everything
-  mutate(mention_status = "all") %>% 
-  filter(article_set == "PMC")
+  mutate(mention_status = "all") 
   # 9 software mentions in PMC article set with every detail
 
 sw_ver_pub <- mentions_p %>% 
+  filter(!article %in% mismatch_list) %>%
   filter(!is.na(version1) & !is.na(publisher1)) %>% 
 # 699 software mentioned with version and publisher together in PMC articles
   mutate(mention_status = "sw-ver-pub") 
 
 sw_ver_url <- mentions_p %>% 
+  filter(!article %in% mismatch_list) %>%
   filter(!is.na(version1) & !is.na(url)) %>% 
 # 36 software mentioned with version and URL in PMC articles
   mutate(mention_status = "sw-ver-url")
 
 sw_pub_url <- mentions_p %>% 
+  filter(!(article %in% mismatch_list)) %>%
   filter(!is.na(publisher1) & !is.na(url)) %>% 
 # 20 software mentioned with publisher and URL in PMC articles
   mutate(mention_status = "sw-pub-url") 
@@ -148,13 +199,13 @@ mention_sample_2 <- bind_rows(sw_ver_pub, sw_ver_url, sw_pub_url) %>%
   sample_n(size=add_2, replace=FALSE) %>% 
   bind_rows(all_mentioned)
 
-set.seed(42)
-rows <- sample(nrow(mention_sample_2))
-mention_sample_2 <- mention_sample_2[rows, ]
+set.seed(23)
+rows <- sample(nrow(mention_sample_1))
+mention_sample_1 <- mention_sample_1[rows, ]
 # repeat the same procedure for another data frame
 
 library(janitor)
-mention_sample_2 %>% 
+mention_sample_1 %>% 
   tabyl(mention_status)
 # write to data/*_mention_sample_tasks.csv
 # use annotation `id` to identify text!
