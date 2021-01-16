1debead8312358be35a613eb9b727fe73f195f88
Fan Du <cfdu@utexas.edu>
2021-01-15 19:28:51 -0600

    prerelease clean up

--- data/json_goldstandard/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in *.json; do
-    if ! grep -qxFe "$i" files-json.txt; then
-        echo "deleting: $i"
-        rm "$i"
-    fi
-done
--- data/tei/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in *.tei.xml; do
-    if ! grep -qxFe "$i" files-xml.txt; then
-        echo "deleting: $i"
-        rm "$i"
-    fi
-done
